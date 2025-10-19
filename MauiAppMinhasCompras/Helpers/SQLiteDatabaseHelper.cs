c10228e014c28d128e63878613559125313626f0
Tiago Casale <casaletiago@gmail.com>
2025-10-19 16:56:48 -0300

    Finalização do básico no desenvolvimento do sistema de compras. Finalizando os métodos:
    - Remover Produtos
    - Selecionar Itens da Lista
    - Editar Produto
    - Adicionar Produto
    - Somar Produtos

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -13,11 +13,13 @@ namespace MauiAppMinhasCompras.Helpers
             _conn.CreateTableAsync<Produto>().Wait();
         }
 
+        // INSERIR PRODUTO | INSERT A PRODUCT | INSERTAR PRODUCTO
         public Task<int> Insert(Produto p) 
         {
             return _conn.InsertAsync(p);
         }
 
+        // ATUALIZAR PRODUTO | UPDATE A PRODUCT | ACTUALIZAR PRODUCTO
         public Task<List<Produto>> Update(Produto p)
         {
             string sql = "UPDATE Produto SET Descricao = ?, Quantidade = ?, Preco = ? WHERE Id = ?";
@@ -25,22 +27,26 @@ namespace MauiAppMinhasCompras.Helpers
             return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
         }
 
+        // DELETAR PRODUTO | DELETE A PRODUCT | ELIMINAR PRODUCTO
         public Task<int> Delete(int id)
         {
             return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
         }
 
+        // LISTAR TODOS OS PRODUTOS | LIST ALL PRODUCTS | LISTAR TODOS LOS PRODUCTOS
         public Task<List<Produto>> getAll() 
         { 
             return _conn.Table<Produto>().ToListAsync();
         }
 
+        // PESQUISAR PRODUTO | SEARCH A PRODUCT | BUSCAR PRODUCTO
         public Task<List<Produto>> Search(string q) 
         {
-            string sql = "SELECT * Produto WHERE Descricao LIKE '%" + q + "%'";
+            string sql = "SELECT * FROM Produto WHERE Descricao LIKE '%" + q + "%'";
 
             return _conn.QueryAsync<Produto>(sql);
         }
 
+   
     }
 }
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,9 +1,124 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
 	}
+
+    // Evento disparado quando a página aparece na tela.
+    protected async override void OnAppearing()
+    {
+        try
+        {
+		    List<Produto> tmp = await App.Db.getAll();
+
+		    tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
+    }
+
+    // Evento disparado quando o usuário clica no botão de adicionar Produto na Toolbar.
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new Views.NovoProduto());
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "Ok");
+		}
+    }
+
+    // Evento disparado quando o usuário pesquisa algum Produto na caixa de busca.
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            string q = e.NewTextValue;
+
+            lista.Clear();
+
+            List<Produto> tmp = await App.Db.Search(q);
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+		catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
+
+    }
+
+    // Evento disparado quando o usuário realiza a Soma na Toolbar.
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O Total é {soma:C}";
+
+		DisplayAlert("Total dos Produtos", msg, "OK");
+
+    }
+
+    // Evento disparado quando o usuário clica em um MenuItem da lista.
+    private async void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            // Vai identificar qual MenuItem foi clicado a partir do 'sender'.
+            MenuItem menuItem = (MenuItem)sender;
+
+            // Então obtém o objeto 'Produto' associado àquela linha da lista.
+            Produto produto_selecionado = (Produto)menuItem.BindingContext;
+
+            // Pede confirmação ao usuário antes de apagar.
+            bool confirmacao = await DisplayAlert("Atenção!", $"Tem certeza que deseja remover o produto '{produto_selecionado.Descricao}'?", "Sim, Remover", "Cancelar"
+            );
+
+            if (confirmacao)
+            {
+                // Remove o item do banco de dados.
+                await App.Db.Delete(produto_selecionado.Id);
+
+                // Remove o item da lista que está sendo exibida na tela.
+                lista.Remove(produto_selecionado);
+
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    // Evento disparado quando o usuário seleciona um item da lista.
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        try
+        {
+            Produto produto_selecionado = e.SelectedItem as Produto;
+
+            Navigation.PushAsync(new Views.EditarProduto
+            {
+                BindingContext = produto_selecionado,
+            });
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "Ok");
+        }
+    }
 }
