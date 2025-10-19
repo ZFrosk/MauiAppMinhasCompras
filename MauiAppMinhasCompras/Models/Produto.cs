d78af8d230520ed985d8c870b773f72891326aff
Tiago Casale <casaletiago@gmail.com>
2025-10-19 17:30:01 -0300

    Validação na inserção de produtos e correção dos produtos duplicados na tela de listagem.

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,14 +4,55 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+        double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
-        public string Descricao { get; set; }
+        public string Descricao 
+        {
+            get => _descricao; 
+            set
+            {
+                if(value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição do Produto");
+                }
 
-        public double Quantidade { get; set; }
+                _descricao = value;
+            }
+        }
 
-        public double Preco { get; set; }
+        public double Quantidade 
+        { 
+            get => _quantidade;
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new Exception("Por favor, preencha a quantidade do Produto");
+                }
+
+                _quantidade = value;
+            }
+
+        }
+
+        public double Preco 
+        { 
+            get => _preco;
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new Exception("Por favor, preencha o preço do Produto");
+                }
+
+                _preco = value;
+            }
+        }
 
         public double Total { get => Quantidade * Preco; }
     }
